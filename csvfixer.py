5df91fe579e559ea698bafe3beb9437842f641d6
pandaninjas <admin@malwarefight.gq>
2022-10-18 21:13:32 -0700

    Fix the csv fixer

--- csvfixer.py
+++ csvfixer.py
@@ -4,7 +4,7 @@ with open('data.csv', newline='') as csvfile:
    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
    for row in reader:
        if len(row) < 6:
-           row.append("no")
+           row.append("yes")
        data.append(row)
 with open('data.csv', 'w') as f:
     for row in data:
